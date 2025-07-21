fbc25438f4440123408d3fc76b4710c3d931019a
yuunha <h000414@naver.com>
2025-07-21 17:37:46 +0900

    feat: NavigationBar 추가

--- typing_game_fe/src/app/page.tsx
+++ typing_game_fe/src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import TypingGame from "../components/TypingPage";
 import TypingPage from "../components/TypingPage";
+import NavigationBar from "@/components/NavigationBar";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
+      <NavigationBar/>
       <TypingPage/>
     </main>
   );
