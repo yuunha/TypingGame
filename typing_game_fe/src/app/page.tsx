104e831c7cfc36ebb6573d6d42233b23ea13ad91
유경 <eeheueklf@gmail.com>
2025-07-23 23:35:52 +0900

    [FE]feat: 홈페이지 버튼 및 styled-component

--- typing_game_fe/src/app/page.tsx
+++ typing_game_fe/src/app/page.tsx
@@ -1,39 +1,138 @@
+'use client';
+
 import Image from "next/image";
+import styled from "styled-components";
 import TypingGame from "../app/_components/TypingGame";
-import TypingPage from "../app/_components/TypingPage";
+import TypingPage from "./(typing)/long/TypingPage";
 import NavigationBar from "../app/_components/NavigationBar";
+import '@/styles/font/font.css';
 
 export default function Home() {
   return (
-    <div className="relative w-full h-screen">
-      {/* 배경 이미지 */}
-      <div
-        className="absolute inset-0 bg-cover bg-center"
-        style={{
-          backgroundImage: "url('/background.jpg')",
-          backgroundColor: "white",
-        }}
-      ></div>
+    <Container>
+      <Background />
 
-      {/* 중앙 네모 박스 */}
-      <div className="relative z-10 flex items-center justify-center h-full">
-        {/* <div className="bg-white/10 backdrop-blur-md border border-white/30 p-10 rounded-2xl shadow-2xl text-center h-[700px] w-[1200px] flex flex-col items-center justify-center"> */}
-        {/* </div> */}
-          {/* 아이콘 그리드 */}
-          <div className="grid grid-cols-4 gap-6 p-10">
-          {Array.from({ length: 12 }).map((_, i) => (
-            <div
-              key={i}
-              className="w-24 h-24 rounded-2xl bg-white/20 backdrop-blur-sm border border-white/30 shadow-inner shadow-white/30 flex items-center justify-center text-sm font-semibold text-white hover:scale-105 transition-transform duration-200"
-            >
-              
-              <span className="text-white font-semibold">앱 {i + 1}</span>
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
+      <SideBox>
+        <IconGrid>
+          <IconBox>
+            <IconText>긴</IconText>
+          </IconBox>
+          <IconBox width="12rem">
+            <IconText>게임</IconText>
+          </IconBox>
+          <IconBox>
+            <IconText>짧</IconText>
+          </IconBox>
+          <BlankBox></BlankBox>
+          <IconBox color="#274A74">
+            <IconText>낱</IconText>
+          </IconBox>
+          <BlankBox></BlankBox>
+          <IconBox color="#274A74">
+            <IconText>랭</IconText>
+          </IconBox>
+        </IconGrid>
+      </SideBox>
+      <CenterBox></CenterBox>
+    </Container>
   );
 }
 
 
+const Container = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100vh;
+`;
+
+const Background = styled.div`
+  position: absolute;
+  inset: 0;
+  background-image: url('/background.jpg');
+  background-size: cover;
+  background-position: center;
+  background-color: white;
+`;
+
+const SideBox = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 300px;
+  transform: translateY(-50%);
+  display: flex;
+  width: 500px;
+`;
+const CenterBox = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 300px;
+  transform: translateY(-50%);
+  display: flex;
+  width: 500px;
+`;
+const IconGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr); // 3열짜리 그리드
+  gap: 20px;
+  width: 300px;
+`;
+
+const BlankBox = styled.div`
+  width: 8rem;
+  height: 8rem;
+`;
+
+const IconBox = styled.div<{ width?: string; color?: string }>`
+  width: ${({ width }) => width || '8rem'};
+  height: 8rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: transform 0.2s ease;
+  cursor: pointer;
+
+  border-radius: 2rem;
+  color: ${({ color }) => color || 'white'};
+  font-size: 1.2rem;
+
+  border: 1px solid rgba(255, 255, 255, 0.5);
+  // background-color: rgba(255, 255, 255, 0.3);
+  backdrop-filter: blur(10px) saturate(120%);
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+
+  // background: rgba(255, 255, 255, 0.15);
+  // backdrop-filter: blur(2px) saturate(180%);
+  // border: 1px solid rgba(255, 255, 255, 0.8);
+  // box-shadow:
+  //   0 8px 32px rgba(31, 38, 135, 0.2),
+  //   inset 0 4px 20px rgba(255, 255, 255, 0.3);
+
+  &::after {
+    content: '';
+    position: absolute;
+    top: 0; left: 0;
+    width: 100%; height: 100%;
+    background: rgba(255, 255, 255, 0.1);
+    border-radius: 2rem;
+    backdrop-filter: blur(1px);
+    box-shadow:
+      inset -10px -8px 0px -11px rgba(255, 255, 255, 1),
+      inset 0px -9px 0px -8px rgba(255, 255, 255, 1);
+    opacity: 1;
+    z-index: -1;
+    filter: blur(1px) drop-shadow(10px 4px 6px white) brightness(115%);
+    pointer-events: none;
+  }
+    
+  &:hover {
+    transform: scale(0.95);
+    // background: rgba(255, 255, 255, 0.1);
+  }
+`;
+
+
+
+const IconText = styled.span`
+  font-size: 5rem;
+  font-weight: 600;
+`;
