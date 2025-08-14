cf4d2c91622cb8d283b4460c1b09db3b88111481
yuunha <h000414@naver.com>
2025-08-14 13:36:49 +0900

    [BE] refactor(mylongtext): request DTO 제약조건 추가

--- typing_game_be/src/main/java/hello/typing_game_be/myLongText/dto/MyLongTextRequest.java
+++ typing_game_be/src/main/java/hello/typing_game_be/myLongText/dto/MyLongTextRequest.java
@@ -1,5 +1,7 @@
 package hello.typing_game_be.myLongText.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,6 +14,11 @@ import lombok.Setter;
 @AllArgsConstructor
 @Builder
 public class MyLongTextRequest {
+    @NotBlank(message = "제목은 필수입니다.")
+    @Size(max = 20, message = "제목은 최대 20자까지 가능합니다.")
     private String title;
+
+    @NotBlank(message = "내용은 필수입니다.")
+    @Size(max = 500, message = "내용은 최대 500자까지 가능합니다.")
     private String content;
 }
