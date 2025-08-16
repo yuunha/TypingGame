7fb3e34b6b9d4dc0bdeb40714e548c4cc1008807
yuunha <h000414@naver.com>
2025-08-16 10:20:50 +0900

    [BE] config(security): CORS 설정 추가
    - 기존 문제 : put,delete 메소드 관련한 cors 에러
    - SecurityFilterChain에 corsConfigurationSource Bean 등록
    - AllowedOrigins, AllowedMethods, AllowedHeaders 설정

--- typing_game_be/src/main/java/hello/typing_game_be/common/security/SecurityConfig.java
+++ typing_game_be/src/main/java/hello/typing_game_be/common/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package hello.typing_game_be.common.security;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -12,6 +14,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import lombok.RequiredArgsConstructor;
 
@@ -39,7 +44,7 @@ public class SecurityConfig {
             )
             .formLogin(form -> form.disable())  // 폼 로그인 비활성화
             .httpBasic(Customizer.withDefaults()) //HTTP Basic 인증 활성화
-            .cors(cors -> {});
+            .cors(cors -> cors.configurationSource(corsConfigurationSource())); // 적용
         return http.build();
     }
     @Bean //인증관리자를 Bean으로 등록
@@ -54,5 +59,17 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
