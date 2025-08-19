4e8b6838c50f628893b65073e1245d4dd191abdc
유경 <eeheueklf@gmail.com>
2025-08-19 13:12:42 +0900

    🧱[FE]setting: CORS 에러로 프록시 설정

--- typing_game_fe/next.config.ts
+++ typing_game_fe/next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return process.env.NODE_ENV === "development"
+      ? [
+          {
+            source: '/api/:path*',
+            destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+          },
+        ]
+      : [];
+  },
 };
 
 export default nextConfig;
