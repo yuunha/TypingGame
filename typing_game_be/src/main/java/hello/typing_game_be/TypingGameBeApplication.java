64e995534f6d6f311ae02801c7980cd9a7297857
yuunha <h000414@naver.com>
2025-09-01 12:08:25 +0900

    [BE] config(s3) : 환경변수 설정 방식 변경
    - AWS S3 관련 환경 변수를 .env 파일에서 읽도록 application.yml에 spring.config.import 추가
      - 기존 dotenv 방식은 JUnit 테스트 환경에서 동작하지 않아 변경
      (JUnit은 main()을 실행하지 않기 때문)
    - 실행 환경별 .env 파일 위치
      - 기본 실행: typing_game/.env
      - 테스트 실행: typing_game_be/.env

--- typing_game_be/src/main/java/hello/typing_game_be/TypingGameBeApplication.java
+++ typing_game_be/src/main/java/hello/typing_game_be/TypingGameBeApplication.java
@@ -11,15 +11,7 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class TypingGameBeApplication {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
 
-        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-        System.setProperty("AWS_USERNAME", dotenv.get("AWS_USERNAME"));
-        System.setProperty("AWS_PASSWORD", dotenv.get("AWS_PASSWORD"));
-        System.setProperty("AWS_REGION", dotenv.get("AWS_REGION"));
-        System.setProperty("AWS_ACCESS_KEY", dotenv.get("AWS_ACCESS_KEY"));
-        System.setProperty("AWS_SECRET_KEY", dotenv.get("AWS_SECRET_KEY"));
 
         SpringApplication.run(TypingGameBeApplication.class, args);
     }
