4dc93aa23aee97fbe30447cecc0a9f8eb8e1138b
yuunha <h000414@naver.com>
2025-10-14 19:43:31 +0900

    [BE] refactor(user): NicknameRequest DTO 클래스 애노테이션 설정

--- typing_game_be/src/main/java/hello/typing_game_be/user/dto/NicknameRequest.java
+++ typing_game_be/src/main/java/hello/typing_game_be/user/dto/NicknameRequest.java
@@ -2,7 +2,13 @@ package hello.typing_game_be.user.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.*;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class NicknameRequest {
     @NotBlank(message = "닉네임은 필수입니다.")
     @Size(max = 9, message = "닉네임은 최대 9자까지 가능합니다.")
