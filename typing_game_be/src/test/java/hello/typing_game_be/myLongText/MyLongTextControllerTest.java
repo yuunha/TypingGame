e07d7bd01e4991872ca91fa0ac26c5567e660a81
yuunha <h000414@naver.com>
2025-12-03 22:43:34 +0900

    [BE] refactor(myLongText,myLongTextScore): myLongText,myLongTextScore 테스트 코드 리팩토링
    - 인증 방식 변경(http basic -> oauth2)에 따른 테스트 코드 리팩토링

--- typing_game_be/src/test/java/hello/typing_game_be/myLongText/MyLongTextControllerTest.java
+++ typing_game_be/src/test/java/hello/typing_game_be/myLongText/MyLongTextControllerTest.java
@@ -1,157 +1,195 @@
 package hello.typing_game_be.myLongText;
 
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.Map;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import hello.typing_game_be.common.security.CustomUserDetails;
+import hello.typing_game_be.friend.repository.FriendRequestRepository;
+import hello.typing_game_be.myLongText.dto.MyLongTextRequest;
+import hello.typing_game_be.myLongText.entity.MyLongText;
+import hello.typing_game_be.myLongText.repository.MyLongTextRepository;
+import hello.typing_game_be.user.entity.User;
+import hello.typing_game_be.user.repository.UserRepository;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 public class MyLongTextControllerTest {
 
-//    @Autowired
-//    private MockMvc mockMvc;
-//    @Autowired
-//    UserService userService;
-//    @Autowired
-//    UserRepository userRepository;
-//    @Autowired
-//    MyLongTextRepository myLongTextRepository;
-//    @Autowired
-//    private FriendRequestRepository friendRequestRepository;
-//    @Autowired
-//    ObjectMapper objectMapper;
-//
-//    @BeforeEach
-//    void beforeEach() {
-//        friendRequestRepository.deleteAll();
-//        myLongTextRepository.deleteAll();
-//        userRepository.deleteAll();
-//
-//        //테스트용 유저 저장
-//        userService.register(   new UserCreateRequest("admin1","testid","1111"));
-//    }
-//    @AfterEach
-//    void afterEach() {
-//        myLongTextRepository.deleteAll();
-//        userRepository.deleteAll();
-//    }
-//
-//    @Test//유저가 긴글 저장
-//    void 나의긴글_저장_성공() throws Exception {
-//        //given
-//        //유저 조회
-//        User user= userRepository.findByLoginId("testid").orElseThrow();
-//
-//        MyLongTextRequest request = MyLongTextRequest.builder()
-//            .title("애국가")
-//            .content("동해물과 백두산이\n")
-//            .build();
-//
-//        //when&then
-//        mockMvc.perform(post("/my-long-text")
-//                .with(httpBasic("testid", "1111"))
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(objectMapper.writeValueAsString(request)))
-//            .andExpect(status().isCreated());
-//
-//        MyLongText myLongText = myLongTextRepository.findByUser(user).get(0);
-//        assertThat(myLongText.getTitle()).isEqualTo("애국가");
-//        assertThat(myLongText.getContent()).isEqualTo("동해물과 백두산이\n");
-//    }
-//    @Test
-//    void 나의긴글_삭제_성공() throws Exception {
-//        //given
-//        //유저 조회
-//        User user= userRepository.findByLoginId("testid").orElseThrow();
-//        //유저가 나의긴글 저장
-//        MyLongText myLongText = myLongTextRepository.save(
-//            MyLongText.builder()
-//                .user(user)
-//                .title("애국가")
-//                .content("동해물과 백두산이\n")
-//            .build()
-//        );
-//        //when&then
-//        mockMvc.perform(delete("/my-long-text/{myLongTextId}",myLongText.getMyLongTextId())
-//            .with(httpBasic("testid", "1111")))
-//            .andExpect(status().isOk());
-//
-//    }
-//    @Test
-//    void 나의긴글_삭제_실패_존재하지않는글_and_권한문제() throws Exception {
-//        //given
-//        //유저 조회
-//        User user= userRepository.findByLoginId("testid").orElseThrow();
-//        //유저가 나의긴글 저장
-//        MyLongText myLongText = myLongTextRepository.save(
-//            MyLongText.builder()
-//                .user(user)
-//                .title("애국가")
-//                .content("동해물과 백두산이\n")
-//                .build()
-//        );
-//        //유저2 저장
-//        userService.register(new UserCreateRequest("admin2","testid2","11112"));
-//
-//        //when&then
-//        mockMvc.perform(delete("/my-long-text/100",myLongText.getMyLongTextId())
-//                .with(httpBasic("testid", "1111")))
-//            .andExpect(status().isNotFound())
-//            .andExpect(jsonPath("$.message").value("존재하지 않는 '나의 긴글'입니다."));
-//
-//        //유저2가 유저1의 글 삭제
-//        mockMvc.perform(delete("/my-long-text/{myLongTextId}",myLongText.getMyLongTextId())
-//            .with(httpBasic("testid2", "11112")))
-//            .andExpect(status().isForbidden());
-//
-//    }
-//    @Test
-//    void 나의긴글목록_조회_성공() throws Exception {
-//        //given
-//        //유저 조회
-//        User user= userRepository.findByLoginId("testid").orElseThrow();
-//        //유저가 나의긴글 저장
-//        myLongTextRepository.save(
-//            MyLongText.builder()
-//                .user(user)
-//                .title("애국가1")
-//                .content("동해물과 백두산이\n")
-//                .build()
-//        );
-//        myLongTextRepository.save(
-//            MyLongText.builder()
-//                .user(user)
-//                .title("애국가2")
-//                .content("동해물과 백두산이\n")
-//                .build()
-//        );
-//        //when&then
-//        mockMvc.perform(get("/my-long-text")
-//                .with(httpBasic("testid", "1111")))
-//            .andExpect(status().isOk())
-//            .andExpect(jsonPath("$[0].title").value("애국가1"))
-//            .andExpect(jsonPath("$[1].title").value("애국가2"));
-//    }
-//    @Test
-//    void 나의긴글_단건조회_성공() throws Exception {
-//        //given
-//        //유저 조회
-//        User user= userRepository.findByLoginId("testid").orElseThrow();
-//        //유저가 나의긴글 저장
-//        MyLongText myLongText = myLongTextRepository.save(
-//            MyLongText.builder()
-//                .user(user)
-//                .title("애국가")
-//                .content("동해물과 백두산이\n")
-//                .build()
-//        );
-//
-//        //when&then
-//        mockMvc.perform(get("/my-long-text/{myLongTextId}",myLongText.getMyLongTextId())
-//                .with(httpBasic("testid", "1111")))
-//            .andExpect(status().isOk())
-//            .andExpect(jsonPath("$.title").value("애국가"))
-//            .andExpect(jsonPath("$.content").value("동해물과 백두산이\n"));
-//    }
+   @Autowired
+   private MockMvc mockMvc;
+   @Autowired
+   UserRepository userRepository;
+   @Autowired
+   MyLongTextRepository myLongTextRepository;
+   @Autowired
+   private FriendRequestRepository friendRequestRepository;
+   @Autowired
+   ObjectMapper objectMapper;
+
+   private Authentication auth_A;
+
+   Authentication registerUser(String nickname, String providerId, String provider) {
+        User user = userRepository.save(User.builder()
+            .nickname(nickname)
+            .providerId(providerId)
+            .provider(provider)
+            .build()
+        );
+        // CustomUserDetails 생성
+        CustomUserDetails customUser = new CustomUserDetails(user, Map.of());
+
+        // Authentication 객체 생성
+        Authentication auth = new UsernamePasswordAuthenticationToken(
+            customUser, null, customUser.getAuthorities()
+        );
+
+        return auth;
+   }
+
+   @BeforeEach
+   void beforeEach() {
+       friendRequestRepository.deleteAll();
+       myLongTextRepository.deleteAll();
+       userRepository.deleteAll();
+
+       auth_A = registerUser("user1","11111","kakao");
+       User user = userRepository.findByNickname("user1").orElseThrow();
+       //테스트용 유저 저장
+   }
+
+   @Test//유저가 긴글 저장
+   void 나의긴글_저장_성공() throws Exception {
+       //given
+       //유저 조회
+       User user= userRepository.findByNickname("user1").orElseThrow();
+
+       MyLongTextRequest request = MyLongTextRequest.builder()
+           .title("애국가")
+           .content("동해물과 백두산이\n")
+           .build();
+
+       //when&then
+       mockMvc.perform(post("/my-long-text")
+               .with(authentication(auth_A))
+               .contentType(MediaType.APPLICATION_JSON)
+               .content(objectMapper.writeValueAsString(request)))
+           .andExpect(status().isCreated());
+
+       MyLongText myLongText = myLongTextRepository.findByUser(user).get(0);
+       assertThat(myLongText.getTitle()).isEqualTo("애국가");
+       assertThat(myLongText.getContent()).isEqualTo("동해물과 백두산이\n");
+   }
+   @Test
+   void 나의긴글_삭제_성공() throws Exception {
+       //given
+       //유저 조회
+       User user= userRepository.findByNickname("user1").orElseThrow();
+       //유저가 나의긴글 저장
+       MyLongText myLongText = myLongTextRepository.save(
+           MyLongText.builder()
+               .user(user)
+               .title("애국가")
+               .content("동해물과 백두산이\n")
+           .build()
+       );
+       //when&then
+       mockMvc.perform(delete("/my-long-text/{myLongTextId}",myLongText.getMyLongTextId())
+           .with(authentication(auth_A)))
+           .andExpect(status().isOk());
+
+   }
+   @Test
+   void 나의긴글_삭제_실패_존재하지않는글_and_권한문제() throws Exception {
+       //given
+       //유저 조회
+       User user= userRepository.findByNickname("user1").orElseThrow();
+       //유저가 나의긴글 저장
+       MyLongText myLongText = myLongTextRepository.save(
+           MyLongText.builder()
+               .user(user)
+               .title("애국가")
+               .content("동해물과 백두산이\n")
+               .build()
+       );
+       //유저2 저장
+       Authentication auth_B=registerUser("admin2","testid2","11112");
+
+       //when&then
+       mockMvc.perform(delete("/my-long-text/100",myLongText.getMyLongTextId())
+           .with(authentication(auth_A)))
+           .andExpect(status().isNotFound())
+           .andExpect(jsonPath("$.message").value("존재하지 않는 '나의 긴글'입니다."));
+
+       //유저2가 유저1의 글 삭제
+       mockMvc.perform(delete("/my-long-text/{myLongTextId}",myLongText.getMyLongTextId())
+           .with(authentication(auth_B)))
+           .andExpect(status().isForbidden());
+
+   }
+   @Test
+   void 나의긴글목록_조회_성공() throws Exception {
+       //given
+       //유저 조회
+       User user= userRepository.findByNickname("user1").orElseThrow();
+       //유저가 나의긴글 저장
+       myLongTextRepository.save(
+           MyLongText.builder()
+               .user(user)
+               .title("애국가1")
+               .content("동해물과 백두산이\n")
+               .build()
+       );
+       myLongTextRepository.save(
+           MyLongText.builder()
+               .user(user)
+               .title("애국가2")
+               .content("동해물과 백두산이\n")
+               .build()
+       );
+       //when&then
+       mockMvc.perform(get("/my-long-text")
+           .with(authentication(auth_A)))
+           .andExpect(status().isOk())
+           .andExpect(jsonPath("$[0].title").value("애국가1"))
+           .andExpect(jsonPath("$[1].title").value("애국가2"));
+   }
+   @Test
+   void 나의긴글_단건조회_성공() throws Exception {
+       //given
+       //유저 조회
+       User user= userRepository.findByNickname("user1").orElseThrow();
+       //유저가 나의긴글 저장
+       MyLongText myLongText = myLongTextRepository.save(
+           MyLongText.builder()
+               .user(user)
+               .title("애국가")
+               .content("동해물과 백두산이\n")
+               .build()
+       );
+
+       //when&then
+       mockMvc.perform(get("/my-long-text/{myLongTextId}",myLongText.getMyLongTextId())
+           .with(authentication(auth_A)))
+           .andExpect(status().isOk())
+           .andExpect(jsonPath("$.title").value("애국가"))
+           .andExpect(jsonPath("$.content").value("동해물과 백두산이\n"));
+   }
 }
--- typing_game_be/src/test/java/hello/typing_game_be/myLongTextScore/MyLongTextScoreControllerTest.java
+++ typing_game_be/src/test/java/hello/typing_game_be/myLongTextScore/MyLongTextScoreControllerTest.java
@@ -1,9 +1,36 @@
 package hello.typing_game_be.myLongTextScore;
 
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import hello.typing_game_be.common.security.CustomUserDetails;
+import hello.typing_game_be.friend.repository.FriendRequestRepository;
+import hello.typing_game_be.myLongText.entity.MyLongText;
+import hello.typing_game_be.myLongText.repository.MyLongTextRepository;
+import hello.typing_game_be.myLongTextScore.dto.MyLongTextScoreRequest;
+import hello.typing_game_be.myLongTextScore.entity.MyLongTextScore;
+import hello.typing_game_be.myLongTextScore.repository.MyLongTextScoreRepository;
+import hello.typing_game_be.user.entity.User;
+import hello.typing_game_be.user.repository.UserRepository;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -11,96 +38,109 @@ import org.springframework.transaction.annotation.Transactional;
 @ActiveProfiles("test")
 public class MyLongTextScoreControllerTest {
 
-//    @Autowired
-//    private MockMvc mockMvc;
-//    @Autowired
-//    private MyLongTextScoreRepository myLongTextScoreRepository;
-//    @Autowired
-//    private UserService userService;
-//    @Autowired
-//    private UserRepository userRepository;
-//    @Autowired
-//    private MyLongTextRepository myLongTextRepository;
-//    @Autowired
-//    private FriendRequestRepository friendRequestRepository;
-//    @Autowired
-//    private ObjectMapper objectMapper;
-//
-//    private User user;
-//    private Long userId;
-//    private MyLongText myLongText;
-//    private Long myLongTextId;
-//
-//    @BeforeEach
-//    void beforeEach() {
-//        //1. DB 초기화
-//        friendRequestRepository.deleteAll();
-//        myLongTextScoreRepository.deleteAll();
-//        myLongTextRepository.deleteAll();
-//        userRepository.deleteAll();
-//
-//        //2. 테스트용 유저 저장 및 ID 저장
-//        userService.register( new UserCreateRequest("admin","testid","1111"));
-//        user = userRepository.findByLoginId("testid").orElseThrow();
-//        userId = user.getUserId();
-//
-//        //3. 긴글 저장 및 ID 저장
-//        myLongTextRepository.save(MyLongText.builder()
-//            .title("나의긴글")
-//            .content("나의긴글입니다..")
-//            .build()
-//        );
-//        myLongText = myLongTextRepository.findByTitle("나의긴글").orElseThrow();
-//        myLongTextId = myLongText.getMyLongTextId();
-//    }
-//    @AfterEach
-//    void afterEach() {
-//        myLongTextScoreRepository.deleteAll();
-//        myLongTextRepository.deleteAll();
-//        userRepository.deleteAll();
-//    }
-//
-//    @Test
-//    void 나의긴글점수_저장_성공() throws Exception {
-//        //given
-//        MyLongTextScoreRequest request = new MyLongTextScoreRequest(500);
-//
-//        //when&then
-//        mockMvc.perform(post("/my-long-text/" + myLongTextId  + "/score")
-//                .with(httpBasic("testid", "1111"))
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(objectMapper.writeValueAsString(request)))
-//            .andExpect(status().isCreated());
-//
-//        List<MyLongTextScore> savedScores = myLongTextScoreRepository.findByUserUserId(userId);
-//        MyLongTextScore savedScore = savedScores.get(0);
-//        assertThat(savedScore.getMyLongText().getTitle()).isEqualTo("나의긴글");
-//        assertThat(savedScore.getScore()).isEqualTo(500);
-//    }
-//    @Test
-//    void 특정_나의긴글에대한_최고점수_조회_성공() throws Exception {
-//        //given
-//        //긴글에 대한 점수 저장
-//        myLongTextScoreRepository.save(
-//            MyLongTextScore.builder()
-//                .user(user)
-//                .myLongText(myLongText)
-//                .score(100)
-//                .build()
-//        );
-//        myLongTextScoreRepository.save(
-//            MyLongTextScore.builder()
-//                .user(user)
-//                .myLongText(myLongText)
-//                .score(200)
-//                .build()
-//        );
-//
-//        //when&then
-//        mockMvc.perform(get("/my-long-text/" + myLongTextId  + "/score")
-//                .with(httpBasic("testid", "1111"))
-//                .contentType(MediaType.APPLICATION_JSON))
-//            .andExpect(status().isOk())
-//            .andExpect(jsonPath("$.score").value(200));
-//    }
+   @Autowired
+   private MockMvc mockMvc;
+   @Autowired
+   private MyLongTextScoreRepository myLongTextScoreRepository;
+   @Autowired
+   private UserRepository userRepository;
+   @Autowired
+   private MyLongTextRepository myLongTextRepository;
+   @Autowired
+   private FriendRequestRepository friendRequestRepository;
+   @Autowired
+   private ObjectMapper objectMapper;
+
+   private User user;
+   private Long userId;
+   private MyLongText myLongText;
+   private Long myLongTextId;
+   private Authentication auth_A;
+
+
+    Authentication registerUser(String nickname, String providerId, String provider) {
+        User user = userRepository.save(User.builder()
+            .nickname(nickname)
+            .providerId(providerId)
+            .provider(provider)
+            .build()
+        );
+        // CustomUserDetails 생성
+        CustomUserDetails customUser = new CustomUserDetails(user, Map.of());
+
+        // Authentication 객체 생성
+        Authentication auth = new UsernamePasswordAuthenticationToken(
+            customUser, null, customUser.getAuthorities()
+        );
+
+        return auth;
+   }
+   @BeforeEach
+   void beforeEach() {
+       //1. DB 초기화
+       friendRequestRepository.deleteAll();
+       myLongTextScoreRepository.deleteAll();
+       myLongTextRepository.deleteAll();
+       userRepository.deleteAll();
+
+       //2. 테스트용 유저 저장 및 ID 저장
+       auth_A = registerUser( "user1","11111","kakao");
+       user = userRepository.findByNickname("user1").orElseThrow();
+       userId = user.getUserId();
+
+       //3. 긴글 저장 및 ID 저장
+       myLongTextRepository.save(MyLongText.builder()
+           .title("나의긴글")
+           .content("나의긴글입니다..")
+           .build()
+       );
+       myLongText = myLongTextRepository.findByTitle("나의긴글").orElseThrow();
+       myLongTextId = myLongText.getMyLongTextId();
+   }
+
+
+   @Test
+   void 나의긴글점수_저장_성공() throws Exception {
+       //given
+       MyLongTextScoreRequest request = new MyLongTextScoreRequest(500);
+
+       //when&then
+       mockMvc.perform(post("/my-long-text/" + myLongTextId  + "/score")
+               .with(authentication(auth_A))
+               .contentType(MediaType.APPLICATION_JSON)
+               .content(objectMapper.writeValueAsString(request)))
+           .andExpect(status().isCreated());
+
+       List<MyLongTextScore> savedScores = myLongTextScoreRepository.findByUserUserId(userId);
+       MyLongTextScore savedScore = savedScores.get(0);
+       assertThat(savedScore.getMyLongText().getTitle()).isEqualTo("나의긴글");
+       assertThat(savedScore.getScore()).isEqualTo(500);
+   }
+
+   @Test
+   void 특정_나의긴글에대한_최고점수_조회_성공() throws Exception {
+       //given
+       //긴글에 대한 점수 저장
+       myLongTextScoreRepository.save(
+           MyLongTextScore.builder()
+               .user(user)
+               .myLongText(myLongText)
+               .score(100)
+               .build()
+       );
+       myLongTextScoreRepository.save(
+           MyLongTextScore.builder()
+               .user(user)
+               .myLongText(myLongText)
+               .score(200)
+               .build()
+       );
+
+       //when&then
+       mockMvc.perform(get("/my-long-text/" + myLongTextId  + "/score")
+               .with(authentication(auth_A))
+               .contentType(MediaType.APPLICATION_JSON))
+           .andExpect(status().isOk())
+           .andExpect(jsonPath("$.score").value(200));
+   }
 }
